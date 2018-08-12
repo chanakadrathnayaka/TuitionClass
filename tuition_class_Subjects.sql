fd2e26abd633b4d37a40919cb986310bf619e972
Chanaka <chanakar@nimbusventure.com>
2018-08-12 23:36:44 +0530

    gitignore file is added

--- tuition_class_Subjects.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tuition_class.Subjects (subjectId, name) VALUES (1, 'Science');
-INSERT INTO tuition_class.Subjects (subjectId, name) VALUES (2, 'Maths');
-INSERT INTO tuition_class.Subjects (subjectId, name) VALUES (3, 'English');
-INSERT INTO tuition_class.Subjects (subjectId, name) VALUES (4, 'Commerce');
-INSERT INTO tuition_class.Subjects (subjectId, name) VALUES (5, 'IT');
